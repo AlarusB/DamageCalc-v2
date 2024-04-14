bafcf6bcf1109397a36b17bd3b4b2f0faa7ee01c
AlarusB <abdul@boateng.org>
2024-04-15 10:44:22 +1200

    added armor piercing

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const spells = {
     blast: (params) => {
         const { level, power, magic, amount, ultimateArt } = params;
-        console.log(amount);
+        // console.log(amount);
         let damage = magic.damage * (level + 19 + power);
         if (amount > 1)
         {
@@ -60,6 +60,9 @@ const spells = {
     }
 }
 
+const MAX_LEVEL = 136;
+const HP_PER_VIT = 4;
+
 function calculateBaseHP(level)
 {
     return 93 + level*7;
@@ -67,8 +70,21 @@ function calculateBaseHP(level)
 
 function calculateVitalityReduction(level, vitality) {
     const baseHealth = calculateBaseHP(level);
-    const vitHealth = baseHealth + vitality*4;
+    const vitHealth = baseHealth + vitality*HP_PER_VIT;
     return Math.sqrt(baseHealth/vitHealth);
+}
+
+function calculateSubStat(value)
+{
+    let val = 0.01 * (1.35 * (( ((16 * Math.log(0.1*value+4)**3)*0.09+(0.15*value)) / (0.1+0.15*MAX_LEVEL**0.5) ) - 0.79 ));
+    return val;
+}
+
+function calculateArmorPiercing(armorPiercing, targetDefense, targetLevel, targetVitality)
+{
+    
+    let increase = calculateSubStat(armorPiercing) * targetDefense / (calculateBaseHP(targetLevel) + (targetVitality*HP_PER_VIT) + targetDefense - targetDefense) / 4;
+    return 1 + increase;
 }
 
 function findStatus(magic)
@@ -123,7 +139,7 @@ function calculateSynergy(magic, targetStatus, existingStatus)
             break;
         }
     }
-    console.log(statusResult);
+    // console.log(statusResult);
     return [synergyDamage, statusResult];
 }
 
@@ -133,21 +149,29 @@ function updateResult()
         level: Number($('#level').val()),
         vitality: Number($('#vitality').val()),
         power: Number($('#power').val()),
+        armorPiercing: Number($('#armorPiercing').val()),
         magic: stats[String($('#magic').val())],
         spell: String($('#spell').val()),
         intensity: Number($('#intensity').val()),
         amount: Number($('#amount').val()),
         charge: Number($('#charge').val()),
         ultimateArt: Boolean($('#ultimateArt').prop('checked')),
-        targetStatus: String($('#targetStatus').val()),
         shape: String($('#shape').val()),
+        targetStatus: String($('#targetStatus').val()),
+        targetLevel: Number($('#targetLevel').val()),
+        targetVitality: Number($('#targetVitality').val()),
+        targetDefense: Number($('#targetDefense').val()),
+        
     };
     // Restrict vitality
     $('#vitality').val(Math.min(params.vitality, params.level*2));
+    $('#targetVitality').val(Math.min(params.targetVitality, params.targetLevel*2));
+
 
     const vitReduction = calculateVitalityReduction(params.level, params.vitality);
+    const piercingBoost = calculateArmorPiercing(params.armorPiercing, params.targetDefense, params.targetLevel, params.targetVitality);
     const spellDamage = spells[params.spell](params);
-    const attackDamage = Math.floor(spellDamage * vitReduction);
+    const attackDamage = Math.floor(spellDamage * vitReduction * piercingBoost);
     const statusName = findStatus(params.magic);
     const dotDamage = calculateDoT({ damage: attackDamage * vitReduction, intensity: params.intensity, statusName: statusName});
 
@@ -167,6 +191,6 @@ function updateResult()
 }
 
 // Input Listeners
-$('#level, #vitality, #power, #intensity, #magic, #spell, #amount, #shape, #ultimateArt, #charge, #targetStatus').on('input', function() {
+$('#level, #vitality, #power, #intensity, #magic, #spell, #amount, #shape, #ultimateArt, #charge, #targetStatus, #targetLevel, #targetDefense, #targetVitality, #armorPiercing').on('input', function() {
     updateResult();
 });
